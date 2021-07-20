78833bc239e54cbd45f2beec047c67eb63e5453f
LeBuoi <nickbui0301@gmail.com>
2021-07-20 22:39:10 +0700

    Changed pom configuration, added Quiz repository code

---
+++ QuizPracticeSystem/src/main/java/com/group04/repositories/LessonRepository.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.group04.repositories;
+
+import com.group04.entities.Lesson;
+import com.group04.entities.Subject;
+import java.util.List;
+
+/**
+ *
+ * @author HP
+ */
+public interface LessonRepository {
+    List<Lesson> getLessonBySubjectId(Long subjectId);
+    public void deleteLesson(Long lessonID);
+    public void updateLesson(Lesson lesson);
+    public void addLesson(Lesson lesson);
+}
---
+++ QuizPracticeSystem/src/main/java/com/group04/repositories/LessonRepositoryImp.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.group04.repositories;
+
+import com.group04.entities.Lesson;
+import com.group04.entities.Subject;
+import com.group04.utiils.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author HP
+ */
+public class LessonRepositoryImp implements LessonRepository{
+
+    @Override
+    public List<Lesson> getLessonBySubjectId(Long subjectId) {
+        Transaction transaction = null;
+        List<Lesson> Lesson = null;
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            Lesson = session.createQuery("FROM Lesson L WHERE L.subjectID = :id").setParameter("id", subjectId)
+                    .getResultList();
+            if (Lesson != null) {
+                return Lesson;
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                System.out.println("Loop Function");
+                transaction.rollback();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteLesson(Long lessonID) {
+        Transaction transaction = null;
+        try ( Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Lesson lesson = session.get(Lesson.class, lessonID);
+            if (lesson != null) {
+                session.delete(lesson);
+                System.out.println("lesson is deleted");
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+        }
+    }
+
+    @Override
+    public void updateLesson(Lesson lesson) {
+        Transaction transaction = null;
+        try ( Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.evict(lesson);
+            session.merge(lesson);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+
+    @Override
+    public void addLesson(Lesson lesson) {
+        Transaction transaction = null;
+        try ( Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.save(lesson);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+    
+    
+    
+    
+}
